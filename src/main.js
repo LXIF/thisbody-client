0d4e93fac668a31e3ccbea65028ad5a43013b1dd
LXIF <andri.schatz@gmail.com>
2021-10-23 17:13:35 +0200

    first production

--- src/main.js
+++ src/main.js
@@ -6,10 +6,7 @@ import store from './store/index'
 import BaseCard from './components/layout/BaseCard'
 import BaseButton from './components/layout/BaseButton'
 import BaseFader from './components/UI/BaseFader'
-import BaseSwipe from './components/UI/BaseSwipe'
 import ConnectionFrame from './components/UI/ConnectionFrame'
-import BaseSwiper from './components/UI/BaseSwiper'
-import BaseCheckbox from './components/UI/BaseCheckbox'
 import BaseMarquee from './components/layout/BaseMarquee'
 import MarqueeButton from './components/UI/MarqueeButton'
 
@@ -18,10 +15,7 @@ createApp(App)
 .component('base-card', BaseCard)
 .component('base-button', BaseButton)
 .component('base-fader', BaseFader)
-.component('base-swipe', BaseSwipe)
 .component('connection-frame', ConnectionFrame)
-.component('base-swiper', BaseSwiper)
-.component('base-checkbox', BaseCheckbox)
 .component('base-marquee', BaseMarquee)
 .component('marquee-button', MarqueeButton)
 
--- src/pages/UserView.vue
+++ src/pages/UserView.vue
@@ -10,21 +10,21 @@
                 v-else
                 lang="en"
                 class='now-touching'
-                @pointerdown='testTone'
+                @pointerdown='testSynth'
                 >Ů̹̟̣̲̥͇͎͚̲̟̅̓̂̂̓͋̂̓͐̿͐́̕̕͟W̸̡̡̬̱̤̬̬̩̫͙̻̠͕͕̣͖͔̾ͭ͑̃̾̀ͦ̏̌͊̚͘U̡͗ͮ̄ͫ͛̂̆̈ͦͫ̈́ͣ̚̚̚͘͝͏͓͕̹̥̮̳̬̬̙̟͈͕͇̗͕͘W̢̡̛̫̞̪̞̲͓̤̲͍̻̦̥̮̮̹͆̓̓̾̎ͨ̊͝Ų̤̥̳͔͍̫ͬ͗̀͑̑́̚͜Ẉ̷̶̨̛̫̖̱̪̬͓͚̜̣̮̙̫̮̬̹̎̀ͯ̓̂ͪͯ͆̇͐ͯ̂ͯ̌̐̋̐ͣ̚ͅÙ̷̘̯͓͔̞͙̟͍͕̠̗̺̫͔͙̮̆͌ͬ̍̑̇͞W̸̛͉̮͖̑ͬͤͩͩͧ͑́̐ͅỤ̵̫͙̘̹̥͎̥̑̂̍̿ͤ̆͑̆̂ͪ͋͞͡͝͡Ẇ̨̡̹̭͈͎̮̜ͥ̂̄͂̌̒ͫͧͩ̋ͨͯ̃ͭ̌̃̓ͅŰ̶͖͉͍̼͔̪̙̼ͩ͗͆͂̽͋ͤ̓ͪ̀̅̅͐ͥ̚͟
 </h3>
          </transition>
         <color-screen id='breath-display'
             :lightness='breath'
             :alpha='breath'
-            @pointerdown='testTone'
+            @pointerdown='testSynth'
         />
         <color-screen id='weather-display'
             :lightness='0'
             :hue='0'
             :saturation='0'
             :alpha='0'
-            @pointerdown='testTone'
+            @pointerdown='testSynth'
         />
         <connection-frame></connection-frame>
     </div>
@@ -44,17 +44,12 @@ export default {
         const store = useStore();
         let noSleep = new NoSleep();
 
-        const breath = computed(() => store.getters.getBreath);
-        const bite = computed(() => store.getters.getBite);
-        const nod = computed(() => store.getters.getNod);
-        const tilt = computed(() => store.getters.getTilt);
-        const heartbeat = computed(() => store.getters.getHeartbeat);
+        
 
-        watch(breath, newValue => console.log('breath', newValue));
-        watch(bite, newValue => console.log('bite', newValue));
-        watch(nod, newValue => console.log('nod', newValue));
-        watch(tilt, newValue => console.log('tilt', newValue));
-        watch(heartbeat, newValue => console.log('heartbeat', newValue));
+        
+
+
+        // const heartbeat = computed(() => store.getters.getHeartbeat);
 
 
 
@@ -74,7 +69,165 @@ export default {
         }
 
 
+        //////////////BREATH SAMPLER/////////
 
+
+        //create sample paths
+        const numberOfBreathSamples = 24;
+        const breathBaseUrl = '/samples/breath/';
+        const breathSamplers = [];
+        const breathVolume = new Tone.Volume();
+        const breathFilter = new Tone.Filter(100, 'lowpass');
+
+
+        breathVolume.connect(breathFilter);
+        breathFilter.toDestination();
+
+        breathVolume.mute = true;
+
+        for(let counter = 1; counter <= numberOfBreathSamples; counter++) {
+            let breathSampleName;
+            if(counter < 10) {
+                breathSampleName = 'thisbody_breath_loud_' + '0' + counter + '.wav';
+            } else {
+                breathSampleName = 'thisbody_breath_loud_' + counter + '.wav';
+            }
+            //create actual samplers
+            const breathSampler = new Tone.Sampler({
+                'C3' : breathSampleName
+            }, () => {
+                console.log('loaded', breathSampleName);
+            },
+                breathBaseUrl
+            );
+            breathSampler.connect(breathVolume);
+            breathSamplers.push(breathSampler);
+        }
+
+        //////////////VERY BAD HOTFIX, FIND AUTO SOLUTION FOR SAMPLE LENGTHS LOL////////
+        const breathSampleLengths = [
+            1.107,
+            1.532,
+            1.668,
+            5.498,
+            3.272,
+            2.908,
+            3.560,
+            1.651,
+            1.730,
+            3.291,
+            1.444,
+            2.468,
+            2.540,
+            1.107,
+            3.162,
+            2.239,
+            1.225,
+            1.920,
+            2.122,
+            0.727,
+            3.338,
+            4.053,
+            2.084,
+            2.521
+        ];
+
+        function playBreathSamples() {
+            const normalPitch = 50;
+            const maxRandomPitchFactor = 4;
+
+            const randomSample = Math.floor(24 * Math.random());
+            const randomPitchFactor = 1 + Math.random() * maxRandomPitchFactor;
+            const randomizedPitch = normalPitch * randomPitchFactor;
+            breathSamplers[randomSample].triggerAttack(randomizedPitch);
+            const sampleLength = breathSampleLengths[randomSample] * 1000 / ((normalPitch * randomPitchFactor) / 130);
+            setTimeout(playBreathSamples, sampleLength);
+            
+        }
+
+        function mapRange(in1, in2, out1, out2, value) {
+            return (value - in1) * (out2 - out1) / (in2 - in1) + out1;
+        }
+
+        const breath = computed(() => store.getters.getBreath);
+        const bite = computed(() => store.getters.getBite);
+        const nod = computed(() => store.getters.getNod);
+        const tilt = computed(() => store.getters.getTilt);
+
+        watch(breath, newValue => {
+            if (newValue <= 3) {
+                breathVolume.mute = true;
+                breathVolume.volume.value = -100;
+                breathFilter.frequency.value = 0;
+            } else {
+                breathVolume.mute = false;
+                breathVolume.volume.value = 0 - (100 - newValue) / 2;
+                const newNewValue = 100 * Math.pow((newValue / 100), 4);
+                breathFilter.frequency.value = mapRange(0, 100, 100, 2000, newNewValue);
+            }
+        });
+        watch(bite, newValue => {
+            breathFilter.Q.value = mapRange(0,100,0.91,20,newValue);
+        });
+
+        
+
+        let nodSpeed = 0;
+        let tiltSpeed = 0;
+        let headSpeed = 0;
+
+        const headOsc = new Tone.Oscillator(300, 'sawtooth').start();
+        const headVolume = new Tone.Volume(-100);
+        headVolume.mute = true;
+        const headDelay = new Tone.FeedbackDelay((Math.random() + 1 * 50)/1000, 0.5);
+
+        
+
+        headOsc.connect(headVolume);
+        headVolume.connect(headDelay);
+        headDelay.toDestination();
+
+        watch(nod, (newValue, oldValue) => {
+            nodSpeed = Math.abs(newValue - oldValue);
+            headSpeed = (nodSpeed + tiltSpeed)/2;
+            console.log(headSpeed);
+            if (headSpeed <= 3) {
+                headVolume.mute = true;
+                headVolume.volume.value = -100;
+            } else {
+                headVolume.mute = false;
+                headVolume.volume.value = 0 - (100 - headSpeed * 2);
+                headOsc.frequency.value = headSpeed * 10;
+            }
+        });
+        watch(tilt, (newValue, oldValue) => {
+            tiltSpeed = Math.abs(newValue - oldValue);
+            headSpeed = (nodSpeed + tiltSpeed)/2;
+            if (headSpeed <= 3) {
+                headVolume.mute = true;
+                headVolume.volume.value = -100;
+            } else {
+                headVolume.mute = false;
+                if (headSpeed >= 30) {
+                    headVolume.volume.value = 0;
+                    headOsc.frequency.value = headSpeed * 15;
+                } else {
+                    headVolume.volume.value = 0 - (100 - headSpeed * 3.3);
+                    headOsc.frequency.value = headSpeed * 10;
+                }
+            }
+        });
+
+
+        
+        
+        // watch(tilt, newValue => console.log('tilt', newValue));
+        // watch(heartbeat, newValue => console.log('heartbeat', newValue));
+
+        // function testSample() {
+        //     let randomSample = Math.floor(24 * Math.random()) + 1;
+        //     breathSamplers[randomSample].triggerAttack('C3');
+        // }
 
 
 
@@ -86,6 +239,7 @@ export default {
             Tone.start()
             .then(() => {
                 Tone.Transport.start();
+                playBreathSamples();
             });
             store.dispatch('startAudio');
             noSleep.enable();
@@ -96,6 +250,7 @@ export default {
             startAudio,
             audioIsActive,
             testTone,
+            testSynth,
             breath
         }
     },
@@ -169,7 +324,7 @@ export default {
         hyphens auto
         padding 50px
         position fixed
-        top 20vh
+        top 35vh
         animation wiggle 10s infinite
         user-select none
 
